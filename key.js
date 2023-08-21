83e70fcc144b66a8520ad4c6cecf97ab52c29748
tài minh vũ nguyễn <vudangducminh@gmail.com>
2023-08-21 14:20:37 +0700

    Update key.js

--- key.js
+++ key.js
@@ -1,11 +1,11 @@
 document.onkeydown = (e) => {
     e = e || window.event;
     if(moveable == true){
-        console.log(e.keyCode);
-        if(e.keyCode == 188) move_left();
-        if(e.keyCode == 191) move_right();              
-        if(e.keyCode == 190) move_down(); 
-        if(e.keyCode == 88) var x = clockwise();
-        if(e.keyCode == 90) counterclockwise();
+        console.log(e.key);
+        if(e.key == ',') move_left();
+        if(e.key == '/') move_right();              
+        if(e.key == '.') move_down(); 
+        if(e.key == 'x') var x = clockwise();
+        if(e.key == 'z') counterclockwise();
     }
 }
